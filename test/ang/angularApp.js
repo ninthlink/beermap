3d25f549931686d1091f168890c61c02306ac43a
alex chousmith <alex@ninthlink.com>
2014-11-06 10:37:03 -0800

    move the locationFactory to app/factories/locations.js

--- test/ang/angularApp.js
+++ test/ang/angularApp.js
@@ -2,7 +2,6 @@
 (function() {
 	angular
 		.module('beerMap', ['ui.router', 'ngAnimate', 'google-maps'.ns()])
-		.factory('locationFactory', locationFactory)
 		.controller('beerMapd', beerMapd)
 		.controller('breweryList', breweryList)
 		.config([
@@ -111,123 +110,6 @@
 			var reload = locationFactory.loadAll();
 			console.log(reload);
 		};
-	}
-	
-	locationFactory.$inject = ['$http'];
-	
-	function locationFactory( $http ) {
-		var beerURL = 'beerdb.json';
-		var o = {};
-		
-		o.beerdbjson = false;
-		o.omarkers = [];
-		
-		o.getJSON = function() {
-			if ( this.beerdbjson === false ) {
-				console.log('loading '+ beerURL);
-				this.beerdbjson = $http.get(beerURL);
-			} else {
-				console.log('loading '+ beerURL + ' from stored version?!');
-			}
-			return this.beerdbjson;
-		};
-		o.loadAll = function( $scope ) {
-			var vm = this;
-			if ( vm.omarkers.length == 0 ) {
-				vm.getJSON().success(function(locs) {
-					var c = locs.length;
-					$scope.markers = [];
-					console.log('loaded '+ c + ' locations');
-					for( var i = 0; i < c; i++ ) {
-						var obj = locs[i];
-						obj.id = i;
-						obj.coords = {
-							latitude: locs[i].latitude,
-							longitude: locs[i].longitude
-						};
-						$scope.boundary = vm.checkBounds( $scope.boundary, locs[i].latitude, locs[i].longitude, false );
-						$scope.markers.push(obj);
-					}
-					angular.forEach($scope.markers, function(marker) {
-						marker.onClick = function() {
-							console.log('onClick '+ marker.id);
-							markerClicked($scope, marker.id);
-						}
-						marker.phoneNumber = function() {
-							return marker.phone.replace('(', '').replace(')', '').replace(/ /i, '').replace(/-/i, '');
-						}
-						marker.closeRight = function() {
-							console.log('close..');
-							$scope.brewon = false;
-							$scope.mapclass = '';
-						}
-					});
-					//console.log('##');
-					//console.log($scope.markers);
-					vm.omarkers = $scope.markers; // save for later
-					console.log('## all '+ $scope.markers.length +' markers loaded : bounds are...');
-					console.log($scope.boundary);
-				})
-				.error(function(err) {
-					console.log('Error loading : '+ err.message);
-					$scope.markers = [];
-					vm.omarkers = [];
-				});
-			}
-			return vm.omarkers;
-		};
-		o.checkBounds = function(bounds, newlat, newlng, ob) {
-			var inbounds = true;
-			if ( bounds.minlat === false ) {
-				bounds.minlat = newlat;
-				inbounds = false;
-			} else {
-				if ( bounds.minlat > newlat ) {
-					bounds.minlat = newlat;
-					inbounds = false;
-				}
-			}
-			if ( bounds.maxlat === false ) {
-				bounds.maxlat = newlat;
-				inbounds = false;
-			} else {
-				if ( bounds.maxlat < newlat ) {
-					bounds.maxlat = newlat;
-					inbounds = false;
-				}
-			}
-			if ( bounds.minlng === false ) {
-				bounds.minlng = newlng;
-				inbounds = false;
-			} else {
-				if ( bounds.minlng > newlng ) {
-					bounds.minlng = newlng;
-					inbounds = false;
-				}
-			}
-			if ( bounds.maxlng === false ) {
-				bounds.maxlng = newlng;
-				inbounds = false;
-			} else {
-				if ( bounds.maxlng < newlng ) {
-					bounds.maxlng = newlng;
-					inbounds = false;
-				}
-			}
-			return ob ? inbounds : bounds;
-		};
-		return o;
-	}
-	
-	function markerClicked( $scope, i ) {
-		console.log('clicked #'+ i + ' !');
-		var m = $scope.markers[i];
-		console.log(m);
-		$scope.mapclass = 'slideleft';
-		$scope.overlayclass = 'col-md-12 overlay-container';
-		$scope.brewon = m;
-		$scope.markers.selected = m.id;
-		$scope.$apply();
 	}
 	
 	breweryList.$inject = ['$scope', '$http', 'locationFactory'];
