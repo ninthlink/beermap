c6668db14488168fef83fbcffd9ffe582a499d56
alex chousmith <alex@ninthlink.com>
2014-11-24 11:07:50 -0800

    clean up express routes & add PUT update

--- packages/articles/server/routes/articles.js
+++ packages/articles/server/routes/articles.js
@@ -10,20 +10,16 @@ var hasAuthorization = function(req, res, next) {
   next();
 };
 
+/**
+ * map our functions from the articles server controller to specific endpoints
+ */
 module.exports = function(Articles, app, auth) {
+  app.get('/articles', articles.all);
+  //.post(auth.requiresLogin, articles.create);
+  app.get('/articles/:articleId', articles.placed);
+  app.put('/articles/:id', articles.updatePlace);
+  //.delete(auth.requiresLogin, hasAuthorization, articles.destroy);
 
-  app.route('/articles')
-    .get(articles.all)
-    .post(auth.requiresLogin, articles.create);
-  app.route('/articles/:articleId')
-    .get(articles.placed);
-	/*
-    .put(auth.requiresLogin, hasAuthorization, articles.update)
-    .delete(auth.requiresLogin, hasAuthorization, articles.destroy);
-
-  app.route('/articles/place/:articleId')
-    .get(articles.placed);
-	*/
   // Finish with setting up the articleId param
   app.param('articleId', articles.article);
 };
