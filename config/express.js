5f03e62bbe1c71a5d04367bf7b1a67b14bc4d13a
alex chousmith <alex@ninthlink.com>
2014-11-25 16:02:24 -0800

    183 lines of code for initializing twitter Streams and checking Places twitter infos when the node app is launched. Oy.

--- config/express.js
+++ config/express.js
@@ -21,6 +21,9 @@ var mean = require('meanio'),
   socketio = require('socket.io'),
   http = require('http'),
   io = socketio(http),
+  mongoose = require('mongoose'),
+  Feed = mongoose.model('Feed'),
+  Place = mongoose.model('Place'),
   config = mean.loadConfig();
   /*
   */
@@ -107,27 +110,214 @@ module.exports = function(app, passport, db) {
   // Connect flash for flash messages
   app.use(flash());
   
-  /*
-  // tweet stream setup?! NOT QUITE YET
-  console.log('socket.io listening on port '+ config.http.port + ' (hopefully)');
-  io.on('connection', function(socket) {
-	console.log('new connection');
-	socket.emit('welcome', { hello: 'world' });
-	socket.on('welcome reply', function(data) {
-		console.log('wwwwwwwelcome replyyyyyyyy');
-		console.log(data);
-		console.log('zzzzzz');
-	});
-  });
-  *
+  /**
+   * FROM HERE DOWN
+   *
+   * is for setting up Feeds streams and twitter things
+   *
+   * this code should be someplace else, but WHERE?!
+   */
+  // twit init
   var t = new twit({
-	consumer_key: config.twitter.clientID,
-	consumer_secret: config.twitter.clientSecret,
-	access_token: config.twitter.access_token,
-	access_token_secret: config.twitter.access_token_secret
+    consumer_key: config.twitter.clientID,
+    consumer_secret: config.twitter.clientSecret,
+    access_token: config.twitter.access_token,
+    access_token_secret: config.twitter.access_token_secret
   });
+  // function for saving a new tweet as a Feed in our DB
+  var saveNewTweet = function( twobj, savecallback ) {
+    console.log( '!!! new tweet from '+ twobj.user.screen_name +' id #'+ twobj.id_str );
+    //console.log(twobj);
+    var img = '';
+    if ( twobj.entities.hasOwnProperty('media') ) {
+      //console.log('-- media attached --');
+      //console.log(twobj.entities.media);
+      if ( twobj.entities.media.length > 0 ) {
+        if ( twobj.entities.media[0].type === 'photo' ) {
+          img = twobj.entities.media[0].media_url_https;
+        }
+      }
+    }
+    
+    var tw_by = {
+      id: twobj.user.id_str,
+      name: twobj.user.name,
+      screen_name: twobj.user.screen_name
+    };
+    /**
+     * NOTE :
+     * currently there is a disconnect here, because the follow stream can contain
+     * https://dev.twitter.com/streaming/overview/request-parameters#follow
+     * tweets created by the user
+     * retweeted by the user
+     * replies to any tweet created by the user
+     * retweets of any tweet created by the user
+     * manual replies created without pressing reply
+     *
+     * in those last cases, the tweet's user object
+     * would be different than the Place twitter account that it was for...
+     * but maybe we just don't care about those ones anyways?
+     */
+    var newfeeditem = new Feed({
+      body: twobj.text,
+      date: twobj.created_at,
+      img: img,
+      origin_id: twobj.id_str,
+      source: {
+        from: 'Twitter',
+        url: 'https://twitter.com/'+ twobj.user.screen_name +'/status/'+ twobj.id_str
+      },
+      author: tw_by
+    });
+    // actually save to db?
+    newfeeditem.save(function(err) {
+      if ( !err ) {
+        savecallback();
+      }
+    });
+  };
+  // step through results and update 1 at a time?
+  var updatePlaceTwitterStep = function(data) {
+    if ( data.length > 0 ) {
+      var firstplace = data.shift();
+      //console.log(firstplace);
+      Place.update({
+        // find all Places where twit.name == twitter object screen_name
+        'twit.name': firstplace.screen_name
+      }, {
+        // and update their twit.user_id & twit.img appropriately
+        'twit.user_id': firstplace.id_str,
+        'twit.img': firstplace.profile_image_url_https
+      }, {
+        multi: true
+      }, function(err, numberAffected, rawResponse) {
+        console.log('saved twitter user_id & user img for '+ numberAffected +' @'+ firstplace.screen_name);
+        // also save the latest status in our Feeds?!
+        if ( firstplace.hasOwnProperty('status') ) {
+          console.log('saving new status for '+ firstplace.screen_name +' ...');
+          // add user object for later tweet save processing?
+          firstplace.status.user = {
+            id_str: firstplace.id_str,
+            name: firstplace.name,
+            screen_name: firstplace.screen_name
+          };
+          saveNewTweet( firstplace.status, function() {
+            // loop
+            updatePlaceTwitterStep( data );
+          });
+        } else {
+          // no status, so skip and just go to saving next place?
+          console.log('no status to save for @'+ firstplace.screen_name +' ...');
+          // loop
+          updatePlaceTwitterStep( data );
+        }
+      });
+    }
+  };
+  // check list of Places that have twitter Names but not twitter user IDs
+  Place
+    .where('twit.name').ne("")
+    .where('twit.img').equals("")
+    .distinct('twit.name', function(err, twitter_names) {
+      if ( err ) {
+        console.log('twit name search err');
+        console.log(err);
+      } else {
+        if ( twitter_names.length > 0 ) {
+          if ( twitter_names.length > 100 ) {
+            console.log('more than 100 places twitter names found. first 100 :');
+            twitter_names = twitter_names.slice(0,100);
+          }
+          
+          var names_str = twitter_names.join();
+          console.log(twitter_names.length +' lookup : '+ names_str + ' : ...');
+          
+          // GET call to /users/lookup to populate any missing user_names & IDs
+          t.get('users/lookup', { screen_name: names_str }, function(err, data, response) {
+            if ( err ) {
+              console.log('twit search err');
+              console.log(err);
+            } else {
+              console.log('*** TWIT SEARCH SUCCESS! data for '+ data.length +' places twitter users : and then?');
+              // and process..
+              updatePlaceTwitterStep( data );
+            }
+          });
+        }
+      }
+    });
+  // query DB for Places with twit.user_id so we can set up our STREAM
+  Place
+    .find({})
+    .where('twit.user_id').ne("")
+    .distinct('twit.user_id', function(err, twitter_ids) {
+      if ( err ) {
+        console.log('twit user_id search err');
+        console.log(err);
+      } else {
+        if ( twitter_ids.length > 0 ) {
+          if ( twitter_ids.length > 100 ) {
+            console.log('more than 100 places twitter names found. first 100 :');
+            twitter_ids = twitter_ids.slice(0,100);
+            // in this case, we'd really have to set up multiple streams...
+          }
+          
+          var ids_str = twitter_ids.join();
+          console.log(twitter_ids.length +' lookup : '+ ids_str + ' : ...');
+          
+          // GET call to /users/lookup to populate any missing user_names & IDs
+          t.get('users/lookup', { user_id: ids_str }, function(err, data, response) {
+            if ( err ) {
+              console.log('twit search err');
+              console.log(err);
+            } else {
+              console.log('*** TWIT SEARCH SUCCESS! data for '+ data.length +' places twitter users : and then?');
+              // and process..
+              updatePlaceTwitterStep( data );
+            }
+          });
+          
+          // STREAM
+          var tstream = t.stream('statuses/filter', { follow: ids_str });
+          // https://github.com/ttezel/twit#event-tweet
+          tstream.on('tweet', function(twobj) {
+            saveNewTweet( twobj, function() {
+              console.log('new feed item saved to database!?');
+              // YESSS : socket emit notify?!
+              //io.sockets.emit('tweet', { screen_name: twobj.user.screen_name, id: twobj.id_str });
+            });
+          });
+          // https://github.com/ttezel/twit#event-delete
+          tstream.on('delete', function(deleteMessage) {
+            console.log('xxxx Twitter Stream delete event xxxx');
+            console.log(deleteMessage);
+            // #todo : actual delete from the DB & socket emit ?! example :
+            /*
+            deleteMessage = {
+              delete: {
+                status: {
+                  id: 537386102920081400,
+                  id_str: '537386102920081409',
+                  user_id: 465999585,
+                  user_id_str: '465999585'
+                },
+                timestamp_ms: '1416958749728'
+              }
+            };
+            */
+          });
+          // https://github.com/ttezel/twit#event-error
+          tstream.on('error', function(error) {
+            console.log('xxxx Twitter Stream error xxxx');
+            console.log(error);
+          });
+          
+          // #todo : cycle through the twitter_ids and make sure they are FOLLOWED and in the list?
+        }
+      }
+    });
   /*
-  // REST call to search all tweets with / from @BPBrewing
+  // example REST call to search all tweets with / from @BPBrewing
   t.get('search/tweets', { q: '@BPbrewing' }, function(err, data, response) {
 	if ( err ) {
 		console.log('twit search err');
@@ -136,13 +326,6 @@ module.exports = function(app, passport, db) {
 		console.log('TWIT SEARCH SUCCESS');
 		console.log(data);
 	}
-  });
-  *
-  // example connecting to a twitter STREAM for every tweet mentioning craftbeer or #craftbeer
-  var tstream = t.stream('statuses/filter', { track: 'craftbeer' });
-  tstream.on('tweet', function(tweet) {
-	console.log( 'tweet from @'+ tweet.user.screen_name +' id #'+ tweet.id_str );
-	io.sockets.emit('tweet', { screen_name: tweet.user.screen_name, id: tweet.id_str });
   });
   */
 };
