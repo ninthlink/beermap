a7b95e3fa7d26ea1deb13654b6b8e5d277e0dcc1
alex chousmith <alex@ninthlink.com>
2014-11-26 14:47:28 -0800

    add the twitter stream delete handler to a static on the Feed Schema ?!

--- packages/articles/server/models/feed.js
+++ packages/articles/server/models/feed.js
@@ -145,15 +145,36 @@ FeedSchema.statics.saveNewTweet = function( twobj, callback ) {
     callback();
   });
 };
-/**
- * given an origin_id, find and remove that tweet?
- */
-FeedSchema.statics.deleteItem = function( og_id, callback ) {
-  this.remove({ origin_id: og_id }, function(err) {
+// tstream.on('delete' sends a twitter deleteMessage object to delete Feed item
+FeedSchema.statics.deleteTweetFromStream = function(deleteMessage) {
+  /*
+  deleteMessage = {
+    delete: {
+      status: {
+        id: 537386102920081400,
+        id_str: '537386102920081409',
+        user_id: 465999585,
+        user_id_str: '465999585'
+      },
+      timestamp_ms: '1416958749728'
+    }
+  };
+  */
+  var tw_id = deleteMessage.delete.status.id_str;
+  console.log('xxxx Twitter Stream delete '+ tw_id);
+  //console.log(deleteMessage);
+  this.deleteItem( tw_id, function() {
+    console.log(' ... '+ tw_id +' deleted');
+    // #todo : socket emit ?!
+  });
+};
+// given an origin_id, find & remove that Feed item
+FeedSchema.statics.deleteItem = function( id, callback ) {
+  this.remove({ origin_id: id }, function(err) {
     if ( err ) {
-      console.log('Error in deleting Feed item with origin_id '+ og_id);
+      console.log('Error in deleting Feed item with origin_id '+ id);
     } else {
-      console.log('Feed item with origin_id '+ og_id +' has been deleted?');
+      console.log('Feed item with origin_id '+ id +' has been deleted?');
     }
     callback();
   });
