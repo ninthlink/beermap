92fd926c30c875700de63db25985a798fa15b130
alex chousmith <alex@ninthlink.com>
2014-11-28 23:12:05 -0800

    allow loading feeds for particular author_id

--- packages/articles/server/routes/articles.js
+++ packages/articles/server/routes/articles.js
@@ -14,12 +14,21 @@ var hasAuthorization = function(req, res, next) {
  * map our functions from the articles server controller to specific endpoints
  */
 module.exports = function(Articles, app, auth) {
+  // API to GET list of all Places
   app.get('/articles', articles.all);
-  //.post(auth.requiresLogin, articles.create);
+  // API to GET details on a single Place
   app.get('/articles/:articleId', articles.placed);
+  // API to Update info for a single Place, via PUT
   app.put('/articles/:id', articles.updatePlace);
-  //.delete(auth.requiresLogin, hasAuthorization, articles.destroy);
-  app.get('/feed', articles.fullFeed);
+  // API to GET the first chunk of most recent Feed items
+  app.get('/feed', articles.loadFeed);
+  app.get('/feed/:id', articles.loadFeed);
+  app.get('/feed/:id/:page/:skip', articles.loadFeed);
+  /*
+  // other things we don't use right now but may want at some point..
+  .post(auth.requiresLogin, articles.create);
+  .delete(auth.requiresLogin, hasAuthorization, articles.destroy);
+  */
   // Finish with setting up the articleId param
   app.param('articleId', articles.article);
 };
