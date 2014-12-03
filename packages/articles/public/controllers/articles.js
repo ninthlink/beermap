0a6cb2c4c2ae04274053ff950f7934e22c23f3a7
alex chousmith <alex@ninthlink.com>
2014-12-03 13:38:14 -0800

    fix $sanitize bug and turn off 1 debug message

--- packages/articles/public/controllers/articles.js
+++ packages/articles/public/controllers/articles.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('mean.articles').controller('ArticlesController', ['$scope', '$rootScope', '$stateParams', '$location', '$window', '$sanitize', 'Global', 'Articles', 'Places', 'Feeds', 'PlacesFromSpreadsheet', 'uiGmapGoogleMapApi',
-  function($scope, $rootScope, $stateParams, $location, $window, $sanitize, Global, Articles, Places, Feeds, PlacesFromSpreadsheet, GoogleMapApi ) {
+angular.module('mean.articles').controller('ArticlesController', ['$scope', '$rootScope', '$stateParams', '$location', '$window', 'Global', 'Articles', 'Places', 'Feeds', 'PlacesFromSpreadsheet', 'uiGmapGoogleMapApi',
+  function($scope, $rootScope, $stateParams, $location, $window, Global, Articles, Places, Feeds, PlacesFromSpreadsheet, GoogleMapApi ) {
     $scope.global = Global;
     // map marker icons?!
     var icon_reddot = '/articles/assets/img/dot-red.png';
@@ -88,12 +88,13 @@ angular.module('mean.articles').controller('ArticlesController', ['$scope', '$ro
       }
       
       if ( $rootScope.hasOwnProperty('highlightPlace') ) {
-        console.log('re-center map on highlight _id:'+ $rootScope.highlightPlace.id +' ' + $rootScope.highlightPlace.nameLongest);
         // re-center map at that place?
         $scope.map.center = {
           latitude: $rootScope.highlightPlace.latitude,
           longitude: $rootScope.highlightPlace.longitude
         };
+        // zoom in a bit?
+        $scope.map.zoom = 12;
       }
       //	Map Styles : not sure how to inject otherwise..
       $scope.styles = [{'featureType':'water','elementType':'all','stylers':[{'hue':'#e9ebed'},{'saturation':-78},{'lightness':67},{'visibility':'simplified'}]},{'featureType':'landscape','elementType':'all','stylers':[{'hue':'#ffffff'},{'saturation':-100},{'lightness':100},{'visibility':'simplified'}]},{'featureType':'road','elementType':'geometry','stylers':[{'hue':'#bbc0c4'},{'saturation':-93},{'lightness':31},{'visibility':'simplified'}]},{'featureType':'poi','elementType':'all','stylers':[{'hue':'#ffffff'},{'saturation':-100},{'lightness':100},{'visibility':'off'}]},{'featureType':'road.local','elementType':'geometry','stylers':[{'hue':'#e9ebed'},{'saturation':-90},{'lightness':-8},{'visibility':'simplified'}]},{'featureType':'transit','elementType':'all','stylers':[{'hue':'#e9ebed'},{'saturation':10},{'lightness':69},{'visibility':'on'}]},{'featureType':'administrative.locality','elementType':'all','stylers':[{'hue':'#2c2e33'},{'saturation':7},{'lightness':19},{'visibility':'on'}]},{'featureType':'road','elementType':'labels','stylers':[{'hue':'#bbc0c4'},{'saturation':-93},{'lightness':31},{'visibility':'on'}]},{'featureType':'road.arterial','elementType':'labels','stylers':[{'hue':'#bbc0c4'},{'saturation':-93},{'lightness':-2},{'visibility':'simplified'}]}];
@@ -166,8 +167,8 @@ angular.module('mean.articles').controller('ArticlesController', ['$scope', '$ro
           refilterThrottle();
           // add map click listener to close the highlight?
           maps.event.addListener( gmapd, 'click', function( event ) {
-            console.log('clicked ::');
-            console.log(event);
+            //console.log('clicked ::');
+            //console.log(event);
             // close the current highlight
             $scope.unhighlight();
             $scope.$apply();
